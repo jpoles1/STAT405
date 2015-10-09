f462cc77b15a90b71dc81717385530d1ab0c1dc9
jpoles1 <jpoles1@gmail.com>
2015-10-09 00:23:12 -0500

    Up to part 2

--- EBIO323/elasticityanalysis.R
+++ EBIO323/elasticityanalysis.R
@@ -6,15 +6,18 @@ source("EBIO323/util.R")
 #Creating Transition Matrices
 ageclass=c("Age 0", "Age 1", "Age 2", "Age 3", "Age 4+")
 survival = c(.5, .6, .91, .99, 0)
+#from assignment 
 fecundity = c(0, .02, .68, .7, .4)
-initialMatrix = createTransitionMatrix(ageclass, survival, fecundity);
+#Testing
+#fecundity = c(0, .02, .68, .7, .4)
+initialMatrix = createTransitionMatrix(ageclass, survival, fecundity)
 #Get initial species counts
 input = "10 20 2 10 15"
 vals = as.numeric(strsplit(input, " ")[[1]])
 initialCounts = vals
 #Plot Future Generations (ggplot)
 meltgen = melt(runSim(initialMatrix, initialCounts, ageclass, 100), id.vars="time")
-ggplot(data=meltgen, aes(x=time, y=value, color=variable))+geom_point()+geom_line()
+ggplot(data=meltgen, aes(x=time, y=value, color=variable))+geom_point()+geom_line()+xlab("Time")+ylab("# of Individuals")
 #3D Elasticity Analysis X=Time; Y=Population Size; Z=Percent Change in Matrix Vals
 nudgevals = 1+(seq(-10, 10, length.out=100)/100)
 rgl.open()
